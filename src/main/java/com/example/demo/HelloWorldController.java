0a4be8796133753a9620801193a3050a832002c4
eehhssaan <ehsan.ayaz@preface.education>
2022-09-26 19:32:04 +0800

    first commit with hello world

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello world from TechNet";
+    }
+
+    @RequestMapping("/goodbye")
+    public String goodbye(){
+        return  "Good Bye from TechNet";
+    }
+}
